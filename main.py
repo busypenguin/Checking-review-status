fa9c6691ee38be555c41381504dba57a5e4bd26d
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-07-25 00:02:11 +0300

    refactor main logic

--- main.py
+++ main.py
@@ -18,25 +18,29 @@ if __name__ == '__main__':
     tg_chat_id = env.str('TG_CHAT_ID')
     dvmn_api_tiken = env.str('DVMN_API_TOKEN')
     bot = telegram.Bot(token=telegram_bot_token)
+    headers = {"Authorization": 'Token '+dvmn_api_tiken}
+    url = 'https://dvmn.org/api/long_polling/'
+    payload = None
+
+    bot.send_message(chat_id=tg_chat_id, text="Бот запущен")
 
     while True:
         try:
-            headers = {"Authorization": 'Token '+dvmn_api_tiken}
-            url = 'https://dvmn.org/api/long_polling/'
-            response_for_checking_timestamp_to_request = get_response(url, headers)
-            if response_for_checking_timestamp_to_request['status'] == 'timeout':
-                timestamp = response_for_checking_timestamp_to_request['timestamp_to_request']
-                payload = {'timestamp': timestamp}
-                response_for_getting_review = get_response(url, headers, payload)
-                new_attempts = response_for_getting_review['new_attempts']
+            response_for_checking_timestamp_to_request = get_response(url, headers, payload)
+            if response_for_checking_timestamp_to_request['status'] == 'found':
+                payload = None
+                new_attempts = response_for_checking_timestamp_to_request['new_attempts']
                 is_negative = new_attempts[0]['is_negative']
                 if is_negative:
                     bot.send_message(chat_id=tg_chat_id, text=f"Преподаватель проверил работу: {new_attempts[0]['lesson_title']}\n\nК сожалению, надо исправить")
                 else:
                     bot.send_message(chat_id=tg_chat_id, text=f"Преподаватель проверил работу: {new_attempts[0]['lesson_title']}\n\nУра, можно делать следующий урок!")
+            else:
+                timestamp = response_for_checking_timestamp_to_request['timestamp_to_request']
+                payload = {'timestamp': timestamp}
 
         except requests.exceptions.ReadTimeout:
-            print('Слишком долгая загрузка')
+            continue
         except requests.exceptions.ConnectionError:
             print('Отсутствует подключение к интернету')
             time.sleep(60)
